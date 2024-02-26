2b98aa373bdc10d5a5231491f112dae6f1ebd1c3
Danil_MK <danilmaksimovih@gmail.com>
2024-02-26 18:10:42 +0300

    Make macro selection gui

---
+++ src/main/java/net/smok/macrofactory/gui/selector/MacroSelectionGui.java
@@ -0,0 +1,98 @@
+package net.smok.macrofactory.gui.selector;
+
+import fi.dy.masa.malilib.gui.GuiBase;
+import net.minecraft.client.gui.DrawContext;
+import net.smok.macrofactory.gui.PositionAlignment;
+import net.smok.macrofactory.gui.Rect;
+import net.smok.macrofactory.gui.RectContainer;
+import net.smok.macrofactory.macros.Macro;
+import net.smok.macrofactory.macros.Module;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MacroSelectionGui extends GuiBase {
+
+    private final static int selectorSize = 20;
+    private final static int containerSpace = 5;
+    private final static int columnWidth = selectorSize + containerSpace;
+
+
+    private final Module module;
+    private final List<MacroWidget> macroWidgets = new ArrayList<>();
+
+
+
+    @Nullable
+    private MacroWidget selectedWidget;
+    private int lastMouseX;
+    private int lastMouseY;
+    private RectContainer rectContainer;
+
+
+    public MacroSelectionGui(Module module) {
+        this.module = module;
+    }
+
+    @Override
+    public void initGui() {
+        super.initGui();
+        List<Macro> macroList = module.getAll();
+
+
+
+        // Create bounds for widgets
+        int maxColumns = Math.min(5, width / columnWidth);
+        int maxLines = Math.min(5, height / columnWidth);
+        if (macroList.size() < 5) maxColumns = macroList.size();
+        if (macroList.size() / 5 < 5) maxLines = macroList.size() / 5 + 1;
+
+        int maxWidth = maxColumns * columnWidth - containerSpace;
+        int minY = height / 2 - maxLines * columnWidth / 2;
+        int minX = width / 2 - maxWidth / 2;
+
+
+        rectContainer = new RectContainer(containerSpace, containerSpace, minX, minX + maxWidth, minY, selectorSize);
+
+        // Fill the rect container with widgets
+        for (int line = 0; line < maxLines; line++) {
+            for (int column = 0, i = 0; column < maxColumns; column++, i++) {
+                Macro macro = macroList.get(i);
+                Rect rect = rectContainer.addRect(new PositionAlignment(true, selectorSize));
+                MacroWidget widget = new MacroWidget(rect.x(), rect.y(), rect.width(), rect.height(), macro);
+                addWidget(widget);
+                macroWidgets.add(widget);
+            }
+            if (line < maxLines - 1) rectContainer.addLine();
+        }
+
+    }
+
+    @Override
+    public void render(DrawContext drawContext, int mouseX, int mouseY, float partialTicks) {
+
+        String text = selectedWidget != null ? selectedWidget.getSelectName() : module.getName();
+        drawContext.drawCenteredTextWithShadow(textRenderer, text, width / 2, rectContainer.getLineY() - 20, -1);
+
+        boolean mouseIsMove = this.lastMouseX != mouseX || this.lastMouseY != mouseY;
+
+        if (!rectContainer.isMouseOver(mouseX, mouseY)) {
+            selectedWidget = null;
+            mouseIsMove = false;
+        }
+
+        for (MacroWidget widget : macroWidgets) {
+            widget.render(mouseX, mouseY, widget == selectedWidget, drawContext);
+            if (mouseIsMove && widget.isMouseOver(mouseX, mouseY)) selectedWidget = widget;
+        }
+        lastMouseX = mouseX;
+        lastMouseY = mouseY;
+    }
+
+    @Override
+    public void close() {
+        super.close();
+        if (selectedWidget != null) selectedWidget.callMacro(client);
+    }
+}
--- src/main/java/net/smok/macrofactory/gui/selector/MacroWidget.java
+++ src/main/java/net/smok/macrofactory/gui/selector/MacroWidget.java
@@ -6,8 +6,12 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.smok.macrofactory.gui.MacroIcons;
 import net.smok.macrofactory.macros.Macro;
+import net.smok.macrofactory.macros.actions.ActionType;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public class MacroWidget extends WidgetBase {
 
@@ -19,26 +23,25 @@ public class MacroWidget extends WidgetBase {
         this.macro = macro;
     }
 
-    public void apply(@NotNull MinecraftClient client) {
-        macro.macroExecute(false, client);
-    }
-
-    public void select() {
-
-    }
-
-    public void deselect() {
-
-    }
 
     @Override
     public void render(int mouseX, int mouseY, boolean selected, DrawContext drawContext) {
 
-        ItemStack stone = Items.STONE.getDefaultStack();
-        drawContext.drawItem(stone, x, y);
-        if (selected)
-        {
-            RenderUtils.drawOutlinedBox(this.x, this.y, this.width, this.height, 0x20C0C0C0, 0xE0FFFFFF);
-        }
+        //ItemStack stone = Items.STONE.getDefaultStack();
+        //drawContext.drawItem(stone, x + width / 2 - 10, y + height / 2 - 10);
+
+        drawContext.drawBorder(x, y, width, height, selected ? 0xE0FAFAFA : 0xE0020202);
+
+        bindTexture(MacroIcons.MACRO_EMPTY_ICON.getTexture());
+        MacroIcons.MACRO_EMPTY_ICON.renderAt(x, y, 0, false, false);
+
+    }
+    @NotNull
+    public String getSelectName() {
+        return macro.getSelectName();
+    }
+
+    public void callMacro(@NotNull MinecraftClient client) {
+        macro.macroExecute(false, client);
     }
 }
