d8eb15f6e7c2ac34bb67144093cd9a50fedf5568
Smok419 <danilmaksimovih@gmail.com>
2025-07-06 10:05:46 +0300

    Add Done button to gui

---
+++ src/main/java/net/smok/macrofactory/gui/ButtonGenericWithoutScroll.java
@@ -0,0 +1,15 @@
+package net.smok.macrofactory.gui;
+
+import fi.dy.masa.malilib.gui.button.ButtonGeneric;
+
+public class ButtonGenericWithoutScroll extends ButtonGeneric {
+
+    public ButtonGenericWithoutScroll(int x, int y, int width, boolean rightAlign, String translationKey, Object... args) {
+        super(x, y, width, rightAlign, translationKey, args);
+    }
+
+    @Override
+    public boolean onMouseScrolledImpl(int mouseX, int mouseY, double horizontalAmount, double verticalAmount) {
+        return true;
+    }
+}
